72875e5ce8762f9e75b7c8b2933bf58776e79231
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2024-01-05 23:17:55 +0100

    Initial Vue support

--- src/main/kotlin/com/github/warningimhack3r/intellijshadcnplugin/backend/ISPScanner.kt
+++ src/main/kotlin/com/github/warningimhack3r/intellijshadcnplugin/backend/ISPScanner.kt
@@ -4,6 +4,7 @@ import com.github.warningimhack3r.intellijshadcnplugin.backend.helpers.FileManag
 import com.github.warningimhack3r.intellijshadcnplugin.backend.sources.ISPSource
 import com.github.warningimhack3r.intellijshadcnplugin.backend.sources.impl.ISPReactSource
 import com.github.warningimhack3r.intellijshadcnplugin.backend.sources.impl.ISPSvelteSource
+import com.github.warningimhack3r.intellijshadcnplugin.backend.sources.impl.ISPVueSource
 import com.intellij.openapi.project.Project
 
 object ISPScanner {
@@ -15,6 +16,7 @@ object ISPScanner {
             when {
                 contents.contains("shadcn-svelte.com") -> ISPSvelteSource(project)
                 contents.contains("ui.shadcn.com") -> ISPReactSource(project)
+                contents.contains("shadcn-vue.com") -> ISPVueSource(project)
                 else -> null
             }
         }
